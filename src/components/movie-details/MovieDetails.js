b2a62079f8dff31154e19295d847e3f223f3dfa4
Gabriel <pc866794@gmail.com>
2022-07-01 19:42:16 -0400

    Added button to go back home in MovieDetails and fixed padding under the image

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import {
   fetchAsyncMovieOrSerieById,
   getSelectedMovieOrSerie,
@@ -26,7 +26,7 @@ function MovieDetails() {
 
   return (
     <div className="p-10 h-screen">
-      <div className="flex flex-col p-5">
+      <div className="flex flex-col">
         <div>
           <img
             src={data.Poster}
@@ -35,18 +35,25 @@ function MovieDetails() {
           />
         </div>
 
-        <div className="p-10text-center">
+        <div className="p-10 text-center text-black dark:text-white">
           <h2 className="pb-2 text-2xl font-bold">
             {data.Title} ({data.Writer})
           </h2>
           <p className="pb-2 text-md">
-            {data.Year}, {data.Country}
+            ({data.Year}, {data.Country})
           </p>
           <p className="pb-2 text-md">{data.Actors}</p>
 
           <hr />
           <p className="pb-2 text-lg mt-4">{data.Plot}</p>
         </div>
+
+        <Link
+          to="/"
+          className="bg-transparent text-black dark:text-white mx-auto rounded border border-black dark:border-white py-2 px-4"
+        >
+          Go home
+        </Link>
       </div>
     </div>
   );
